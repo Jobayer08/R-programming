ad6cabbce60c2a8c9412304583a9b569ee5f202b
Jobayer <mdabduljobayer570@gmail.com>
2025-07-09 02:22:47 +0600

    dplyr package

---
+++ lec_7.R
@@ -0,0 +1,27 @@
+#install dplyr
+install.packages("dplyr")
+
+#load libraries
+library(dplyr)
+library(gapminder)
+
+?dplyr
+
+head(gapminder)
+
+#extract column by select
+select(gapminder,country)
+select(gapminder,country,continent)
+select(gapminder,-country)
+select(gapminder,-country,-continent)
+
+#get column by partial maching of column names
+select(gapminder,starts_with("c"))
+select(gapminder,ends_with("p"))
+select(gapminder,contains("co"))
+
+#extract row by filter
+temp=filter(gapminder,country!="Afghanistan")
+temp1=filter(gapminder,country %in% c("Afghanistan","Australia"))
+temp3=filter(gapminder,year>=1952,lifeExp>=40)
+            
