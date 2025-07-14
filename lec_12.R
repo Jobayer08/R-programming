d3d02538d6a44a8b96bf1e753089607fb06d1243
Jobayer <mdabduljobayer570@gmail.com>
2025-07-14 19:40:20 +0600

    R visualization

---
+++ lec_12.R
@@ -0,0 +1,21 @@
+# install pheatmap
+install.packages("pheatmap")
+
+library(pheatmap)
+
+#make a data frame/matrix
+head(mtcars)
+class(mtcars)
+
+df=scale(mtcars)
+
+pheatmap(df,
+         border_color = "red",
+         cellwidth = 20,
+         cluster_rows = T,
+         cutree_rows = 3,
+         show_rownames = T,
+         fontsize = 8,
+         display_numbers = T)
+
+pheatmap(mtcars,scale = "column")
