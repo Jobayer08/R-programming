87c3d215071b6880373e5d08c4cdc27ba3cd67a2
Jobayer <mdabduljobayer570@gmail.com>
2025-07-15 02:22:15 +0600

    ggplot

---
+++ lec_15.R
@@ -0,0 +1,32 @@
+install.packages("tidyverse")
+library(tidyverse)
+library(gapminder)
+
+#bar
+head(gapminder)
+ggplot(gapminder,aes(continent))+
+  geom_bar()
+
+#use of pipe in ggplot
+gapminder%>%
+  filter(year==1952)%>%
+  ggplot(aes(continent))+
+  geom_bar()
+
+#plot population
+head(gapminder)
+gapminder%>%
+  filter(year==1952)%>%
+  ggplot(aes(continent,pop))+
+  geom_bar(stat = "identity")
+
+gapminder%>%
+  filter(year==1952)%>%
+  ggplot(aes(continent,pop))+
+  geom_col()
+
+#stacked bar
+gapminder%>%
+  filter(year>=1952& year<=1970)%>%
+  ggplot(aes(year,pop))+
+  geom_bar(stat = "identity",aes(fill = continent))
