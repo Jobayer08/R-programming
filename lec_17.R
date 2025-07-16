eeb2d443ed54cf604f786cff7d985b38ab7e14a2
Jobayer <mdabduljobayer570@gmail.com>
2025-07-16 14:12:37 +0600

    Data visualization

---
+++ lec_17.R
@@ -0,0 +1,29 @@
+library(tidyverse)
+library(gapminder)
+head(gapminder)
+
+
+#boxplot
+ggplot(gapminder,aes(continent,lifeExp))+
+  geom_boxplot()
+
+#violin
+ggplot(gapminder,aes(continent,lifeExp,fill = continent))+
+  geom_violin(draw_quantiles = c(.25,.5,.75))
+
+#jitter
+
+ggplot(gapminder,aes(continent,lifeExp))+
+  geom_jitter(alpha=.2)+
+  geom_violin(draw_quantiles = c(.25,.5,.75))
+
+#
+gapminder$continent=factor(gapminder$continent,levels = c("Oceania",'Europe','Americas','Asia','Africa'))
+gapminder$continent
+
+
+#jitter
+
+ggplot(gapminder,aes(continent,lifeExp))+
+  geom_jitter(alpha=.2)+
+  geom_violin(draw_quantiles = c(.25,.5,.75))
